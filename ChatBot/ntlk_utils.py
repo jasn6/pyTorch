2091d316e800a373741c15c50788a9968bd0e203
jasn6 <110363877+jasn6@users.noreply.github.com>
2023-04-07 22:57:09 -0400

    Bag of words implementation

--- ChatBot/ntlk_utils.py
+++ ChatBot/ntlk_utils.py
@@ -1,5 +1,6 @@
 import nltk
-nltk.download('punkt')
+import numpy as np
+# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -9,6 +10,26 @@ def tokenize(sentence):
 def stem(word):
     return stemmer.stem(word.lower())
 
+def bag_of_words(tokenized_sentence,all_words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
+
+'''
 test = "How long does shipping take?"
 print(test)
 test = tokenize(test)
@@ -18,3 +39,4 @@ test2 = ["organize","organizing","organizes"]
 print(test2)
 test2 = [stem(word) for word in test2]
 print(test2)
+'''
